7d8c496cbf856f0b390b0822c247d04e13bd2e91
yao-creative <yytanwork@gmail.com>
2026-08-04 18:31:46 +0800

    Stop tracking target directory

--- app/target/debug/deps/app-300e16946e5febe6.d
+++
@@ -1,5 +0,0 @@
-/Users/yao/projects/mini-codex/app/target/debug/deps/app-300e16946e5febe6.d: src/main.rs
-
-/Users/yao/projects/mini-codex/app/target/debug/deps/libapp-300e16946e5febe6.rmeta: src/main.rs
-
-src/main.rs:
--- app/target/debug/deps/app-d502ce052ce2bdef.d
+++
@@ -1,5 +0,0 @@
-/Users/yao/projects/mini-codex/app/target/debug/deps/app-d502ce052ce2bdef.d: src/main.rs
-
-/Users/yao/projects/mini-codex/app/target/debug/deps/libapp-d502ce052ce2bdef.rmeta: src/main.rs
-
-src/main.rs:
